210ce30877bb4ec00713bccb126a66c326212b95
scifnar <43652962+scifnar@users.noreply.github.com>
2018-10-03 11:57:07 -0400

    Delete AddADuser.ps1

--- AddADuser.ps1
+++
@@ -1,6 +0,0 @@
-﻿#Add Active Directory UserID to group "CS All Campus Services"
-
-$UserList = "C:\temp\UserList.txt"
-$User = Get-Content $UserList
-
-Add-ADGroupMember -Identity "GROUP NAME" -Members $User
